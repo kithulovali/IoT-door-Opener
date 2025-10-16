173b4e78d1dfa43dfd81b2566a3322bfa8021cea
kithulovali <kithulovalibin@gmail.com>
2025-10-17 04:46:51 +0800

    project config

--- DoorOpenner/WebServer/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers 
-from .models import ImagesAccess 
-
-class ImagesAcessSerializers(serializers.ModelSerializer):
-    class Meta :
-        model = ImagesAccess
-        fields = ['id', 'owner', 'email', 'images']
